2897f5b23ce42c8b83e59d86620b8ee42b5723ec
Scott Hood <scottdothood@gmail.com>
2023-10-04 16:06:24 +0100

    add build scripts

---
+++ build.sh
@@ -0,0 +1,5 @@
+rm docs/*
+cp static/* docs/
+elm make src/Main.elm --optimize --output=docs/elm.js
+npx uglifyjs docs/elm.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | npx uglifyjs --mangle --output docs/elm.min.js
+rm docs/elm.js
